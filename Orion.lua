614e72a9eb852fa5ae0ff3e8d3c4f28aa3853132
Hads9 <pjacinto1249@gmail.com>
2024-11-17 01:49:14 -0300

    Update Orion.lua

--- Orion.lua
+++ Orion.lua
@@ -12,10 +12,10 @@ local OrionLib = {
 	Flags = {},
 	Themes = {
 		Default = {
-			Main = Color3.fromRGB(0, 0, 255),
+			Main = Color3.fromRGB(0, 0, 0),
 			Second = Color3.fromRGB(32, 32, 32),
-			Stroke = Color3.fromRGB(0, 0, 255),
-			Divider = Color3.fromRGB(0, 0, 255),
+			Stroke = Color3.fromRGB(255, 0, 0),
+			Divider = Color3.fromRGB(0, 0, 0),
 			Text = Color3.fromRGB(255, 0, 0),
 			TextDark = Color3.fromRGB(150, 150, 150)
 		}
